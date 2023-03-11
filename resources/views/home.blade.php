8dc2789ede868689676c331babc69dad862a8ecd
fafyfun <jainudeenf007@gmail.com>
2023-03-11 12:14:14 +0530

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@if (Auth::user() && Auth::user()->is_admin)
+    <p>You are logged in as an administrator.</p>
+@endif
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
